f9c388df482bb828bb73597c047dcd1d28324770
bquach189 <bquach18@csu.fullerton.edu>
2024-10-20 15:48:23 -0700

    fixed UI, user sent messages, working on response

--- assistant/views.py
+++ assistant/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 
 def assistant(request):
+    if request.method == 'POST':
+        message = request.POST.get('msg')
+        response = 'Hi there'
+        return JsonResponse
     return render(request, 'assistant.html')
