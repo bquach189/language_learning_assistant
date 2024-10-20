f68ca66b5602464c74b0e1c6603b54f329814ea5
bquach189 <bquach18@csu.fullerton.edu>
2024-10-20 10:38:15 -0700

    linked UI html files

--- language_learning_assistant/settings.py
+++ language_learning_assistant/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'assistant'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'language_learning_assistant.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'ui'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
