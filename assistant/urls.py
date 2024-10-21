68aa50b28a7412b51dff1a454ecddd7505458ea8
bquach189 <bquach18@csu.fullerton.edu>
2024-10-21 13:24:50 -0700

    temp

--- assistant/urls.py
+++ assistant/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.assistant, name = 'assistant')
+    path('assistant', views.assistant, name = 'assistant'),
+    path('register', views.register, name = 'register'),
+    path('login', views.login, name = 'login'),
+    path('logout', views.logout, name = 'logout'),
 ]
--- assistant/views.py
+++ assistant/views.py
@@ -1,27 +1,57 @@
+import os
 from django.shortcuts import render
 from django.http import JsonResponse
-import openai as ai
+from django.contrib import auth
+import openai
+from openai import OpenAI
 
+openai.api_key = os.environ['OPENAI_API_KEY']
 
-openai_api_key = 'sk-sxb0f2PmDVq9wkd1SESxA95whY5ZjGqghfovxVJA2cT3BlbkFJ3XJA81bbKIDCBJz1cPhfLoC6fJRY2Yl2CyXNN0VXQA'
+client = OpenAI()
 
-def call_ai(msg):
-    response = ai.Completion.create(
-        model = 'gpt-3.5-turbo-0125',
-        prompt = msg,
-        max_tokens = 100,
-        n = 1,
-        stop = None,
-        temperature = 0.7,
+def register(request):
+    if request.method == 'POST':
+        user = request.POST.get('username', '')
+        email = request.POST.get('email', '')
+        pw = request.POST.get('pw', '')
+        confirm_pw = request.POST.get('confirm_pw', '')
+        if pw == confirm_pw:
+            try:
+                pass
+            except:
+                pass
+        else:
+            error_msg = 'Please make sure both passwords match.'
+            return render(request, 'register.html', {'error_msg': error_msg})
+            
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+
+def call_ai(msg, client_instance):
+        response = client_instance.chat.completions.create(
+        model = 'gpt-3.5-turbo',
+        messages = [
+            { "role": "system", "content": "You are a Language Learning Assistant Chatbot." },
+            {"role": "user", "content": msg},       
+        ],
+        max_tokens = 200,
+        temperature = 0.2,
     )
-    final = response.choice[0].text
-    return final
-    
+        final = response.choices[0].message.content
+        return final
 # Create your views here.
 
 def assistant(request):
     if request.method == 'POST':
         msg = request.POST.get('msg')
-        response = call_ai(msg)
+        response = call_ai(msg, client)
         return JsonResponse({'msg': msg, 'response': response})
     return render(request, 'assistant.html')
+
+
+    
