987a03ebd133011e9b0bd0587d198ddccfce6148
bquach189 <bquach18@csu.fullerton.edu>
2024-10-20 23:01:49 -0700

    finished linking OpenAI GPT

--- assistant/views.py
+++ assistant/views.py
@@ -1,27 +1,30 @@
+import os
 from django.shortcuts import render
 from django.http import JsonResponse
-import openai as ai
+import openai
+from openai import OpenAI
 
+openai.api_key = os.environ['OPENAI_API_KEY']
 
-openai_api_key = 'sk-sxb0f2PmDVq9wkd1SESxA95whY5ZjGqghfovxVJA2cT3BlbkFJ3XJA81bbKIDCBJz1cPhfLoC6fJRY2Yl2CyXNN0VXQA'
+client = OpenAI()
 
-def call_ai(msg):
-    response = ai.Completion.create(
-        model = 'gpt-3.5-turbo-0125',
-        prompt = msg,
-        max_tokens = 100,
-        n = 1,
-        stop = None,
-        temperature = 0.7,
+def call_ai(msg, client_instance):
+        response = client_instance.chat.completions.create(
+        model = 'gpt-3.5-turbo',
+        messages = [
+            { "role": "system", "content": "You are a Language Learning Assistant Chatbot." },
+            {"role": "user", "content": msg},       
+        ],
+        max_tokens = 200,
+        temperature = 2,
     )
-    final = response.choice[0].text
-    return final
-    
+        final = response.choices[0].message.content
+        return final
 # Create your views here.
 
 def assistant(request):
     if request.method == 'POST':
         msg = request.POST.get('msg')
-        response = call_ai(msg)
+        response = call_ai(msg, client)
         return JsonResponse({'msg': msg, 'response': response})
     return render(request, 'assistant.html')
