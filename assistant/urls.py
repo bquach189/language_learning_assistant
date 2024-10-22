1dab8606e454811765e73f6ef37aed6954288d60
bquach189 <bquach18@csu.fullerton.edu>
2024-10-21 22:11:12 -0700

    updated urls and register

--- assistant/urls.py
+++ assistant/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('assistant', views.assistant, name = 'assistant'),
-    path('register', views.register, name = 'register'),
+    path('', views.register, name = 'register'),
     path('login', views.login, name = 'login'),
     path('logout', views.logout, name = 'logout'),
 ]
