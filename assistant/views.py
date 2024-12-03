f4777f7353229f65bcfcc83f54ddbf916c067fdf
bquach189 <bquach18@csu.fullerton.edu>
2024-12-02 22:50:16 -0800

    improved context retention, updated ui

--- assistant/views.py
+++ assistant/views.py
@@ -55,7 +55,7 @@ def logout(request):
 
 
 def get_previous_chats(user):
-    return Chat.objects.filter(user=user).order_by('-created_at')[:8]
+    return Chat.objects.filter(user=user).order_by('-created_at')[:10]
 
 
 def create_context(previous_chats):
@@ -64,21 +64,29 @@ def create_context(previous_chats):
         context += f":User {chat.msg}\nAssistant: {chat.response}\n"
     return context
 
+
+
 def call_ai(context, msg, client_instance):
         prompt = context + f":User  {msg}\nAssistant:"
         response = client_instance.chat.completions.create(
         model = 'gpt-3.5-turbo',
         messages = [
             { "role": "system", "content": "You are a Language Learning Assistant Chatbot. "
-                "You are here to help users learn a new language, please politely refuse to respond to off-topic requests."
+                "Respond to anything language related. Politely refuse to answer anything that isn't language related."
+                "When asked to provide feedback, ensure you give feedback to the precise topic the user is referring to."
+                "When asked to explain a grammar rule, please provide a clear and concise explanation."
+                "When asked to translate a sentence, please provide the translation and a brief explanation of any nuances."
+                "When practicing conversation, please provide feedback on the user's grammar at the end of the conversation."
+                "When asked to provide quizzes for languages, please provide 5 questions, giving correct answers and explanations after each question."
+                "When the user provides answers for the quiz, ensure you are giving answers for the most recent quiz, and count the number of correct answers out of 5."
                 "Be polite and friendly." },
             {"role": "user", "content": prompt},       
         ],
-        max_tokens = 200,
+        max_tokens = 500,
         temperature = 0.2,
     )
         final = response.choices[0].message.content
-        return final
+        return final.replace('\n', '\n')
 
 
 def assistant(request):
